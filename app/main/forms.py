4f3904a552c7ab6cfc2ffdfc3b5722efa08f65c8
3808050054 <zjt380850054@gmail.com>
2018-06-29 10:43:02 +0800

    添加了一个用户中心界面

---
+++ app/main/forms.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,TextAreaField,PasswordField,BooleanField
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+
+class WriteForm(FlaskForm):
+    name = StringField(validators=[Required()])
+    text = TextAreaField(validators=[Required()])
+    submit = SubmitField(u'发帖')
+
+class CommentForm(FlaskForm):
+    text = TextAreaField()
+    submit = SubmitField(u'回复')
+
+class DelForm(FlaskForm):
+    submit = SubmitField(u'删除')
+
+class LoginForm(FlaskForm):
+    username = StringField('username')
+    password = PasswordField('Password',validators=[Required()])
+    submit = SubmitField('Log In')
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email')
+    username = StringField('Username')
+    password = PasswordField('Password')
+    password2 = PasswordField('Confirm password')
+    submit = SubmitField('Register')
+
