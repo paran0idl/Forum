bab950cb013a123a06bc2757bbe82473e43189fb
3808050054 <zjt380850054@gmail.com>
2018-06-30 15:14:58 +0800

    update

--- app/main/views.py
+++ app/main/views.py
@@ -31,6 +31,7 @@ def logout():
 
 @main.route('/',methods = ['get','post'])
 def index():
+    #db.create_all()
     uid=''
     uname=''
     if not current_user.is_anonymous:
@@ -39,12 +40,13 @@ def index():
     return render_template('index.html',u_id=uid,user_name=uname)
 
 @main.route('/<u_id>',methods=['get','post'])
+@login_required
 def user_view(u_id):
     user=User.query.filter_by(u_id=u_id).first()
-    following=Follow.query.filter_by(following_id=u_id).all()
+    followed=Follow.query.filter_by(followed_id=u_id).all()
     posts=[]
-    for f in following:
-        for post in Post.query.filter_by(publisher_id=f.following_id).all():
+    for f in followed:
+        for post in Post.query.filter_by(publisher_id=f.followed_id).all():
             posts.append(post)
     for post in posts:
         print post.title
@@ -276,21 +278,6 @@ def user_center():
     fan_cnt = len(Follow.query.filter_by(Follow.followed_id==current_user.u_id).first())
     posts = Post.query.filter_by(Post.publisher_id==current_user.u_id).first()
     return render_template('user_center.html',name=name,followed=followed,followed_cnt=followed_cnt,fan=fan_cnt,posts=posts)
-
-'''
-@main.route('/user_info')
-@login_required
-def user_info():
-    form = UserInfo()
-    if form.validate_on_submit():
-        result = User.query.filter_by(User.user_name == current_user.user_name).first()
-        result.user_name=form.username.data
-        result.user_pwd=form.newpasswd.data
-        result.user_email=form.email.data
-        db.session.commit()
-        return render_template('index.html')
-    return render_template('user_info.html',form=form)
-'''
 
 @main.route('/focus')
 @login_required
